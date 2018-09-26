e73721bf522bfaa9e97860338484ef49aeb35670
ipochinili <ipochinili12qwas>
2018-09-27 01:00:26 +0300

    Подготовка каркаса проекта

--- edit.php
+++ edit.php
@@ -14,16 +14,16 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <h1>Create task</h1>
+            <h1>Edit task</h1>
             <form action="">
                 <div class="form-group">
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" placeholder="Измените заголовок">
                 </div>
                 <div class="form-group">
-                    <textarea name="" class="form-control"></textarea>
+                    <textarea name="" class="form-control" placeholder="Измените текст"></textarea>
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-success" type="submit">Submit</button>
+                    <button class="btn btn-warning" type="submit">Submit</button>
                 </div>
             </form>
         </div>
